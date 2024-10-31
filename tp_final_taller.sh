b81fd9a184824eeeef8a9a47d5972c09e0b65e8c
victoria-rivaspa13 <162721841+victoria-rivaspa13@users.noreply.github.com>
2024-10-31 09:31:03 -0300

    Update tp_final_taller.sh

--- tp_final_taller.sh
+++ tp_final_taller.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 curl -o server_log.txt https://gist.githubusercontent.com/sebiglesias/ea2faa92f4b25a79f811104584e91efb/raw/02378f041ae64d3d021031efeb1572cbddfc2fc7/test-web-server-log.txt
 
-grep " 200 " server-log.txt >> ok.txt
-
-grep " 500 " server-log.txt >> errors.txt
+cat server-log.txt | grep " 200 " >> ok.txt
+cat server-log.txt | grep " 500 " >> errors.txt
 
 echo "Archivos ok.txt y errors.txt generados."
